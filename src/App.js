2f0de254e0c7eb7b9b61f3b18d69297cff79e1c9
Pablo Henrique <pabloenosteteucpx@gnail.com>
2022-04-06 16:24:22 -0300

    testando

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import Form from './Form/Form';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-black">
-          alo alo pessoal <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Technical Share</h1>
+      <Form />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Form/Button.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Button = () => {
+  return <button>Entrar</button>;
+};
+
+export default Button;
