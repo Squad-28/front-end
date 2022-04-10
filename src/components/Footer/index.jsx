fc6c99f1ec070bb0fc33797483ef915855a6b53f
Lucas M <lucas.msantos68@senacsp.edu.br>
2022-04-10 17:17:14 -0300

    components/Footer: realoca Footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+const Footer = () => {
+  return (
+    <div className="">
+      <p>todos os direitos reservados</p>
+    </div>
+  );
+};
+
+export default Footer;
