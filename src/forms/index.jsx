4c57c290dbf53080cfde5a32fc69c2a9116467e0
Lucas M <lucas.msantos68@senacsp.edu.br>
2022-04-10 17:19:05 -0300

    forms: realoca forms

---
+++ src/forms/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Button from "./Button";
+import Input from "./Input";
+
+const Form = () => {
+  return (
+    <div>
+      <Input id="email" label="email" requerid />
+      <Input id="password" type="password" label="Senha" />
+      <Button />
+    </div>
+  );
+};
+
+export default Form;
