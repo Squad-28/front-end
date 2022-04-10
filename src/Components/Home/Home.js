52c0442ecb72341c031efa28c6e0a8e9840677e9
Pablo Henrique <pabloenosteteucpx@gnail.com>
2022-04-10 15:04:39 -0300

    aletracao

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Perfil from '../../Pages/Perfil/Perfil';
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Perfil />
+    </div>
+  );
 }
 
 export default Home;
---
+++ src/Pages/Perfil/Perfil.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Perfil = () => {
+  return (
+    <div>
+      <div>
+        <h1>Bem vindo!!</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Perfil;
