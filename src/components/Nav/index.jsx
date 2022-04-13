1e60cf0fa81c0e7cef0304ac4f114393357ae9ff
Rafael Borges <rafaelborges.av@gmail.com>
2022-04-13 08:58:53 -0300

    update Nav

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -8,12 +8,23 @@ const Nav = ({ onLogout }) => {
       <div className="grid justify-items-center">
         <div className=" container flex justify-between">
           <h1 className="logo py-6">Technical Share</h1>
-          <div className="nav-items flex items-center px-14 gap-10">
-            <p>Home</p>
-            <img src={FotoPerfil} alt="foto de pefil" width={42} />
-            <p>
-              Olá, <span id="nomePerfil">Clarisa!</span>
-            </p>
+          <div className="nav-items flex  items-center px-14">
+            <ul className="flex flex-row items-center gap-10">
+              <li>
+                <p>Home</p>
+              </li>
+              <li>
+                <p>quero ser mentor</p>
+              </li>
+              <li>
+                <img src={FotoPerfil} alt="foto de pefil" width={42} />
+              </li>
+              <li>
+                <p>
+                  Olá, <span id="nomePerfil">Clarisa!</span>
+                </p>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
