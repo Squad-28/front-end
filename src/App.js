891482ad673de51de204ca7b20effb1af9df0c72
Lucas M <lucas.msantos68@senacsp.edu.br>
2022-04-14 14:15:20 -0300

    src/App.js: adiciona as rotas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from "react";
-import AppRoutes from "./AppRoutes";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
+import PerfilPage from "./pages/PerfilPage/index";
+import CadastroPage from "./pages/CadastroPage/index";
 
 function App() {
-  return <AppRoutes />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/profile" element={<PerfilPage />} />
+        <Route path="/register" element={<CadastroPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
