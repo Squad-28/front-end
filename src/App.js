174932d22342a68b03a89b434603e3d694b7fe30
Lucas M <lucas.msantos68@senacsp.edu.br>
2022-04-14 14:36:57 -0300

    src/App.js: configura getUsers

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
+import { useEffect, useState } from "react";
+
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+
+import { getUsers, getUser } from "./services/api";
 
 import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
@@ -6,11 +10,34 @@ import PerfilPage from "./pages/PerfilPage/index";
 import CadastroPage from "./pages/CadastroPage/index";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  const loadData = async () => {
+    try {
+      
+      const response = await getUsers();
+
+      const { users } = response.data;
+      setUsers(users);
+
+    } catch (err) {
+      console.error(err);
+
+    }
+  };
+
+  useEffect(() => {
+    (async () => await loadData())();
+  }, []);
+
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/"
+          element={<HomePage users={users} />}
+        />
         <Route path="/profile" element={<PerfilPage />} />
         <Route path="/register" element={<CadastroPage />} />
       </Routes>
