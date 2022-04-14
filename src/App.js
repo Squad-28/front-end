35a8843b86e724b5d104d79f207ecf7546c56b7b
Lucas M <lucas.msantos68@senacsp.edu.br>
2022-04-14 15:02:28 -0300

    src/App.js: configura getUser

--- src/App.js
+++ src/App.js
@@ -11,23 +11,48 @@ import CadastroPage from "./pages/CadastroPage/index";
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [userProfile, setUserProfile] = useState([]);
 
   const loadData = async () => {
     try {
-      
       const response = await getUsers();
 
       const { users } = response.data;
       setUsers(users);
-
     } catch (err) {
       console.error(err);
-
     }
   };
 
   useEffect(() => {
     (async () => await loadData())();
+  }, []);
+
+  const handleProfile = async (user) => {
+    const userInArr = Object.assign([user]);
+
+    const userP = await userInArr;
+
+    //console.log(userP);
+
+    setUserProfile(userP);
+  };
+
+  const loadProfile = async (userProfile) => {
+    try {
+      const response = await getUser(userProfile);
+      // console.log(response);
+      const { user } = response.data;
+      // console.log(user);
+      setUserProfile(user);
+      // console.log(userPerfil);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    (async () => await loadProfile(userProfile))();
   }, []);
 
   return (
@@ -36,9 +61,17 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route
           path="/"
-          element={<HomePage users={users} />}
+          element={<HomePage users={users} onProfilel={handleProfile} />}
         />
-        <Route path="/profile" element={<PerfilPage />} />
+        <Route
+          path="/profile"
+          element={
+            <PerfilPage
+              userPerfil={userProfile}
+              setUserPerfil={setUserProfile}
+            />
+          }
+        />
         <Route path="/register" element={<CadastroPage />} />
       </Routes>
     </Router>
