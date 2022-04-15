6de3d196308b4184ac1d568ec8ebe55879fb9438
Lucas M <lucas.msantos68@senacsp.edu.br>
2022-04-14 22:03:47 -0300

    pages/HomePage/Search: adiciona botão, corrige falhas

--- src/pages/HomePage/Search/index.jsx
+++ src/pages/HomePage/Search/index.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
-import './style.css';
+import React, { useState } from "react";
+
+import addIcon from "./assets/add-icon.svg";
+
+import "./style.css";
 
 const Search = ({ onSearch }) => {
-  const [query, setQuery] = useState('');
-  const [tech, setTech] = useState('');
+  const [query, setQuery] = useState("");
 
   return (
     <div className="grid justify-items-center py-20 pl-28">
@@ -11,20 +13,23 @@ const Search = ({ onSearch }) => {
         <h1>Quero ajuda!</h1>
         <h2 className="pt-10">Quais as habilidades necessárias?</h2>
         <p>Adicione as habilidades ou/e selecione abaixo</p>
-        <input
-          className="my-5"
-          type="text"
-          name="query"
-          id="query"
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-        />
+        <div className="search-input">
+          <img src={addIcon} alt="adicionar tecnologia" className="search-add-icon" />
+          <input
+            className="my-3"
+            type="text"
+            name="query"
+            id="search-query"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+        </div>
 
-        <div className="techs">
+        <div className="search-techs">
           <ul>
             <li>Figma</li>
             <li>Adobe XD</li>
-            <li>HTML</li>
+            <li>HTML5</li>
             <li>Scrum</li>
             <li>UI</li>
             <li>javascript</li>
@@ -33,9 +38,11 @@ const Search = ({ onSearch }) => {
           </ul>
         </div>
 
-        <div className="urgencia mt-20">
+        <div className="search-urgency mt-20">
           <h2>Qual o seu nível de urgência?</h2>
-          <p className="mb-5">Os perfils abaixo serão filtrados com base na sua urgência</p>
+          <p className="mb-5">
+            Os perfils abaixo serão filtrados com base na sua urgência
+          </p>
           <div className="flex flex-col">
             <span>
               <a>Quero ajuda agora !</a>
