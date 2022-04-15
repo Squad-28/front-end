44ee210d4a072746687bbb44c5e8a02ee39be717
Lucas M <lucas.msantos68@senacsp.edu.br>
2022-04-15 12:48:55 -0300

    pages/LoginPage, RegisterPage: validação de forms

--- src/pages/RegisterPage/index.jsx
+++ src/pages/RegisterPage/index.jsx
@@ -1,12 +1,53 @@
+/* eslint-disable no-unused-vars */
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+import { Link } from "react-router-dom";
+
 import FotoCadastro from "./assets/fotocadastro.svg";
 import addIcon from "./assets/add-icon.svg";
 
 import NavLogin from "../../components/NavLogin";
 
 import "./style.css";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+
+const registerSchema = yup
+  .object({
+    name: yup.string().required("O nome é obrigatório."),
+    position: yup.string().required("O cargo é obrigatório."),
+    email: yup
+      .string()
+      .email("Digite um email válido.")
+      .required("O email é obrigatório."),
+    password: yup
+      .string()
+      .min(8, "A senha deve ter pelo menos 8 digitos.")
+      .required("A senha é obrigatória."),
+  })
+  .required();
 
 const Register = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(registerSchema),
+  });
+  
+  const [userName, setUserName] = useState("");
+  const [userPosition, setUserPosition] = useState("");
+  const [userEmail, setUserEmail] = useState("");
+  const [userPassword, setUserPassword] = useState("");
+  const [userSkills, setUserSkills] = useState([]);
+
+  const onRegister = (userData) => {
+    console.log(userData);
+  };
+
   return (
     <div>
       <NavLogin />
@@ -23,29 +64,47 @@ const Register = () => {
         <div className="register-content">
           <h1>Cadastro</h1>
           <p>Cadastrar sua conta</p>
-          <form action="">
+          <form onSubmit={handleSubmit(onRegister)}>
             <div className="register-field">
-              <label htmlFor="Nome">Nome:</label>
-              <input type="text" name="Nome" id="Nome" />
+              <label htmlFor="name">Nome:</label>
+              <input
+                type="text"
+                id="name"
+                {...register("name", { required: true })}
+              />
+              <span>{errors.name?.message}</span>
             </div>
             <div className="register-field">
-              <label htmlFor="Cargo">Cargo:</label>
-              <input type="text" name="Cargo" id="Cargo" />
+              <label htmlFor="position">Cargo:</label>
+              <input
+                type="text"
+                id="position"
+                {...register("position", { required: true })}
+              />
+              <span>{errors.position?.message}</span>
             </div>
             <div className="register-field">
-              <label htmlFor="Teams">Seu e-mail do Teams:</label>
-              <input type="email" name="Teams" id="Teams" />
+              <label htmlFor="email">Seu e-mail do Teams:</label>
+              <input
+                type="email"
+                id="email"
+                {...register("email", { required: true })}
+              />
+              <span>{errors.email?.message}</span>
             </div>
             <div className="register-field">
-              <label htmlFor="Senha">Senha</label>
-
-              <input type=" password" name="Senha" id="Senha" />
+              <label htmlFor="password">Senha</label>
+              <input
+                type="password"
+                id="password"
+                {...register("password", { required: true })}
+              />
+              <span>{errors.password?.message}</span>
             </div>
             <div className="register-field">
-              <label htmlFor="Habilidades">Habilidades</label>
-
+              <label htmlFor="skills">Habilidades</label>
               <div className="register-field-addIcon">
-                <input type="text" name="Habilidades" id="Habilidades" />
+                <input type="text" id="skills" />
                 <img src={addIcon} alt="" className="register-addIcon" />
               </div>
 
